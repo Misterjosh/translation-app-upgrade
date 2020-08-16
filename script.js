34d1d748f953d8b0e1dcc4cfaaa7b78a2c296e6f
Josh Brooks <defaultjosh@hotmail.com>
2020-08-15 22:45:40 -0400

    Clear button functionality done. Removed excess console logs

--- script.js
+++ script.js
@@ -5,17 +5,24 @@ $(document).ready(() => {
 $("#translate-button").on("click", () => {
     // get the selected language value
     const selLanguage = $("#langVal").val();
-    console.log(selLanguage);
     // get the input text value 
     const inputText = $("#inputText").val();
-    console.log(inputText);
     // call function for API call
-    makeCall(selLanguage, inputText)
+    translateCall(selLanguage, inputText);
 
 }); // closing brackets for translate button
 
-// make api call
-makeCall = (language, text) => {
+// clear all button
+$("#clear-button").on("click", () => {
+    // clear the input text
+    $("#inputText").val("");
+    // clear the translation
+    $("#outputText").val("");
+
+}); // closing bracket for clear button
+
+// function to make api call
+translateCall = (language, text) => {
     $.ajax({
         url: "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200328T161314Z.bcd4843be4019ae7.a951bdf2b7f5fbb7821f0f8421561bb28404dea0&lang=" + language + "&text=" + text,
         method: "GET"
