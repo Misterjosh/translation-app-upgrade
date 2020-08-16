68b295cd63d782520b8170b39f3338909a92c215
Josh Brooks <defaultjosh@hotmail.com>
2020-08-15 21:58:28 -0400

    Fixed issue: language was always Afrikaans. Gets input text

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // Make sure the page loads before the JavaScript
 $(document).ready(() => {
 
-    const language = $("#langVal").val()
-
+// Translate button functionality
 $("#translate-button").on("click", () => {
+    // get the selected language value
+    const language = $("#langVal").val();
     console.log(language);
+    // get the input text value 
+    const inputText = $("#inputText").val();
+    console.log(inputText);
+
 }); // closing brackets for translate button
 
 }); // closing brackets for document.ready
